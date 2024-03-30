961cfd73811256a51f33b64e78b4af9901bf7ac4
suraj <surajj3404@gmail.com>
2024-03-30 11:00:21 +0530

    add config

---
+++ internal/config.go
@@ -0,0 +1,38 @@
+package config
+
+import (
+	"encoding/json"
+	"io/ioutil"
+	"os"
+)
+
+type Config struct {
+	Seed   string `json:"seed"`
+	ApiURL string `json:"api_url"`
+	Size   int    `json:"size"`
+	AppID  int    `json:"app_id"`
+	Dest   string `json:"dest"`
+	Amount uint64 `json:"amount"`
+	NodeURL string `json:"node_url"`
+}
+
+func (c *Config) GetConfig(configFileName string) error {
+
+	jsonFile, err := os.Open(configFileName)
+	if err != nil {
+		return err
+	}
+	defer jsonFile.Close()
+
+	byteValue, err := ioutil.ReadAll(jsonFile)
+	if err != nil {
+		return err
+	}
+
+	err = json.Unmarshal(byteValue, c)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
